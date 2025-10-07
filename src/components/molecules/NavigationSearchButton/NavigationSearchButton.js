c94c6d28881c666f1811276b901e22089d3f09d8
beskider <michal.code.make@gmail.com>
2025-10-07 09:54:21 +0200

    added detecting click outside search list

--- src/components/molecules/NavigationSearchButton/NavigationSearchButton.js
+++ src/components/molecules/NavigationSearchButton/NavigationSearchButton.js
@@ -4,6 +4,8 @@ import { StyledNavigationSearchButton, NavigationSearchButtonWithIcon, Navigatio
 import { SearchResultsList } from '../SearchResultsList/SearchResultsList';
 import { FaSearch } from 'react-icons/fa';
 
+import { useEffect, useRef } from 'react';
+
 export const NavigationSearchButton = () => {
   
   const [ isOpen, setIsOpen ] = useState(false)
@@ -11,11 +13,27 @@ export const NavigationSearchButton = () => {
   const [ searchResults, setSearchResults ] = useState([])
   
   const { resorts } = useContext(ResortContext);
-
+  
+  const wrapperRef = useRef(null);
+  
   const closeSearch = () => {
     setSearchText('')
     setSearchResults([])
     setIsOpen(false)
+  }
+
+  const useOutsideAlerter = ref => {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          closeSearch();
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
   }
 
   const toggleOpen = () => ( isOpen ? closeSearch() : setIsOpen(true) ) 
@@ -36,15 +54,18 @@ export const NavigationSearchButton = () => {
       setSearchResults(searchedResorts)
   }
 
+  useOutsideAlerter(wrapperRef)
+
   return (
     <>
-      <StyledNavigationSearchButton $isOpen={isOpen}>
+      <StyledNavigationSearchButton $isOpen={isOpen} ref={wrapperRef}>
         <NavigationSearchButtonWithIcon onClick={toggleOpen}>
           <FaSearch/>
         </NavigationSearchButtonWithIcon>
         { 
           isOpen && 
           <NavigationSearchInput $isOpen={isOpen}
+            type="search"
             value={seachText} 
             onChange={handleSearchChange} 
             placeholder="Szukaj ośrodka..."
@@ -53,7 +74,10 @@ export const NavigationSearchButton = () => {
         { 
           searchResults && 
           searchResults.length > 0 && 
-          <SearchResultsList results={searchResults} closeSearch={() => closeSearch()}/>
+          <SearchResultsList 
+            results={searchResults} 
+            closeSearch={() => closeSearch()}
+          />
         }
       </StyledNavigationSearchButton>
 
